34241468190a823a09738ea1279f26e4db120549
AntonKilmetov <86476500+AntonKilmetov@users.noreply.github.com>
2021-06-28 10:20:15 +0500

    PercolationThreshold
    Finding the percolation threshold

---
+++ PercolationThreshold/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace PercolationThreshold
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Red;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(640, 640);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(445, 646);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(77, 53);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Увеличить p на 0.025";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(528, 646);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 53);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(111, 669);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 26);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 646);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(298, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Наличие соединяющего кластера";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(646, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 29);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "P∞ = ";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox3.Location = new System.Drawing.Point(726, 13);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 26);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(646, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 29);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "S   = ";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox4.Location = new System.Drawing.Point(726, 75);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 26);
+            this.textBox4.TabIndex = 8;
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(857, 711);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Порог перколяции";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ PercolationThreshold/Form1.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace PercolationThreshold
+{ 
+    public partial class Form1 : Form
+    {
+        private bool[,] field; // Заполнение ячеек
+        private int[,] markedField; // Маркировка заполненых ячеек
+        private List<int> np; // Ссылки на коллизии
+        private HashSet<int> marckerOfConnectingCluster;
+        private Dictionary<int, int> distribution;
+        private static double p = 0.1; // Вероятность заполнить ячейку
+        private static int sideLength = 16; // Длина стороны L
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// Рисуется перколяционная картина + определяется наличие соединяющего кластера
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = p.ToString();
+            FillField();
+            DetectConnectingCluster();
+            PInfinity();
+            FillDistribution();
+            MediumSizeDistribution();
+            Draw();
+        }
+        /// <summary>
+        /// Заполнения ячеек в соответсвии с вероятностью P
+        /// </summary>
+        private void FillField() 
+        {
+            var random = new Random();
+            field = new bool[sideLength, sideLength];
+            for (int i = 0; i < sideLength; i++)
+                for (int j = 0; j < sideLength; j++)
+                    field[i, j] = random.NextDouble() < p;
+        }
+        /// <summary>
+        /// Увеличить вероятность заполнения Р и перерисовать перколяционную картину
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            p += 0.025;
+            textBox1.Text = p.ToString();
+            FillField();          
+            DetectConnectingCluster();
+            PInfinity();
+            FillDistribution();
+            MediumSizeDistribution();
+            Draw();
+        }
+        /// <summary>
+        /// Отрисовка перколяционной картины
+        /// </summary>
+        private void Draw()
+        {
+            var bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            var graph = Graphics.FromImage(bmp);
+            var whiteBrush = new SolidBrush(Color.White);
+            var width = pictureBox1.Width / sideLength;
+            var height = pictureBox1.Height / sideLength;
+            for (int i = 0; i < sideLength; i++)
+            {
+                for (int j = 0; j < sideLength; j++)
+                {
+                    if (field[i, j])
+                        graph.FillRectangle(
+                            new SolidBrush(Color.FromArgb(markedField[i, j] * 13 % 254, markedField[i, j] * 53 % 254, markedField[i, j] * 2 % 254)),
+                            j * width,
+                            i * height,
+                            width - 1,
+                            height - 1);
+                    else
+                        graph.FillRectangle(whiteBrush, j * width, i * height, width - 1, height - 1);
+                }
+            }
+            pictureBox1.Image = bmp;
+        }
+        /// <summary>
+        /// Информирует о наличие соединяющего кластера 
+        /// </summary>
+        private void DetectConnectingCluster()
+        {
+            marckerOfConnectingCluster = new HashSet<int>();
+            HoshenKopelmanAlgorithm();
+            for (int i = 0; i < sideLength; i++)
+                for (int j = 0; j < sideLength; j++)
+                    if (markedField[0, i] == markedField[sideLength - 1, j] && markedField[0, i] != -1)
+                        marckerOfConnectingCluster.Add(markedField[0, i]);
+            if (marckerOfConnectingCluster.Count == 0) textBox2.Text = "Нет";
+            else textBox2.Text = "Есть";
+        }
+        /// <summary>
+        /// Алгоритм Хошена - Копельмана
+        /// </summary>
+        private void HoshenKopelmanAlgorithm()
+        {
+            markedField = new int[sideLength, sideLength];
+            np = new List<int>();
+            var marker = 0;
+            for (int i = 0; i < sideLength; i++)
+                for (int j = 0; j < sideLength; j++)
+                    markedField[i, j] = -1;
+            for (int i = sideLength - 1; i >= 0; i--)
+            {
+                for (int j = 0; j < sideLength; j++)
+                {
+                    if (i == sideLength - 1 && j == 0) // начальный узел
+                    {
+                        if (field[i, j])
+                        {
+                            markedField[i, j] = marker;
+                            np.Add(-1);
+                            marker++;
+                        }
+                    }
+                    else if (i == sideLength - 1) // нижние узлы
+                    {
+                        if (field[i, j])
+                        {
+                            if (field[i, j - 1])
+                                markedField[i, j] = markedField[i, j - 1];
+                            else
+                            {
+                                markedField[i, j] = marker;
+                                np.Add(-1);
+                                marker++;
+                            }
+                        }
+                    }
+                    else if (j == 0) // левые узлы
+                    {
+                        if (field[i, j])
+                        {
+                            if (field[i + 1, j])
+                                markedField[i, j] = markedField[i + 1, j];
+                            else
+                            {
+                                markedField[i, j] = marker;
+                                np.Add(-1);
+                                marker++;
+                            }
+                        }
+                    }
+                    else // остальные узлы
+                    {
+                        if (field[i, j])
+                        {
+                            if (field[i + 1, j] && field[i, j - 1])
+                            {
+                                if (markedField[i + 1, j] == markedField[i, j - 1])
+                                    markedField[i, j] = markedField[i + 1, j];
+                                else
+                                {
+                                    markedField[i, j] = Math.Min(markedField[i + 1, j], markedField[i, j - 1]);
+                                    np[Math.Max(markedField[i + 1, j], markedField[i, j - 1])]
+                                        = Math.Min(markedField[i + 1, j], markedField[i, j - 1]);
+                                    Relabel(); // Если есть коллизия происходит перемаркировка
+                                }
+                            }
+                            else if (field[i + 1, j])
+                                markedField[i, j] = markedField[i + 1, j];
+                            else if (field[i, j - 1])
+                                markedField[i, j] = markedField[i, j - 1];
+                            else
+                            {
+                                markedField[i, j] = marker;
+                                np.Add(-1);
+                                marker++;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        /// <summary>
+        /// Перемаркировка в случае коллизии
+        /// </summary>
+        private void Relabel()
+        {
+            for (int i = np.Count - 1; i >= 0; i--)
+                if (np[i] != -1)
+                {
+                    for (int j = 0; j < sideLength; j++)
+                        for (int k = 0; k < sideLength; k++)
+                            if (markedField[j, k] == i)
+                                markedField[j, k] = np[i];
+                    np[i] = -1; 
+                }
+        }
+        private void PInfinity()
+        {
+            var count = 0;
+            foreach (var item in markedField)
+            {
+                if (marckerOfConnectingCluster.Contains(item))
+                    count++;
+            }
+            if (count != 0)
+                textBox3.Text = ((double)count / (sideLength * sideLength)).ToString();
+            else
+                textBox3.Text = "0";
+        }
+        private void FillDistribution()
+        {
+            distribution = new Dictionary<int, int>();
+            foreach (var item in markedField)
+            {
+                if (item < 0) continue;
+                if (distribution.ContainsKey(item))
+                    distribution[item]++;
+                else
+                    distribution.Add(item, 1);
+            }
+        }
+        /// <summary>
+        /// Распределение среденего размера кластеров = ns
+        /// </summary>
+        private void MediumSizeDistribution()
+        {
+            var n = sideLength * sideLength;
+            var s = new int[n]
+                .Select((x, y) => y + 1)
+                .ToArray();
+            var ns = new double[n];
+            for (int i = 0; i < n; i++)
+                foreach (var item in distribution.Values)
+                    if (s[i] == item)
+                        ns[i]++;
+            ns = ns
+                .Select(x => x / n)
+                .ToArray();
+            textBox4.Text = AverageClusterSize(s,ns).ToString();
+            //ToFile(s, ns);
+        } 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="s">Возможное количество ячеек кластера</param>
+        /// <param name="ns">Распределение среденего размера кластеров</param>
+        /// <returns>Средний размер кластера</returns>
+        private double AverageClusterSize(int[] s, double[] ns)
+        {
+            var numerator = 0.0;
+            var denominator = 0.0;
+            for (int i = 0; i < ns.Length; i++)
+            {
+                numerator += s[i] * s[i] * ns[i];
+                denominator += s[i] * ns[i];
+            }
+            return numerator / denominator;
+        }
+        private void ToFile(int[] s, double[] ns)
+        {
+            var newS = s.Select(x => x.ToString()).ToArray();
+            var newNS = ns.Select(x => x.ToString()).ToArray();
+            File.WriteAllLines(@"path", newS);
+            File.WriteAllLines(@"path", newNS);
+        }
+    }
+}
